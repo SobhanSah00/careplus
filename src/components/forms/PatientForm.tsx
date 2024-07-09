b99568dd4e24b361214bf23df4a9b24179a5d2d6
sobhan9012 <sobhansahoo2000@gmail.com>
2024-07-09 10:11:31 +0530

    serverral action performed in auth

--- src/components/forms/PatientForm.tsx
+++ src/components/forms/PatientForm.tsx
@@ -1,17 +1,16 @@
-"use client"
- 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
-import {Form,} from "@/components/ui/form"
-import CustomFormProvider from "../CustomFormProvider"
-import SubmitButton from "../SubmitButton"
-import { useState } from "react"
-import { UserFormSchema} from "@/lib/validation"
-import { useRouter } from "next/navigation"
-import { createUser } from "@/lib/actions/patient.actions"
+"use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import CustomFormProvider from "../CustomFormProvider";
+import SubmitButton from "../SubmitButton";
+import { useState } from "react";
+import { UserFormSchema } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -23,51 +22,45 @@ export enum FormFieldType {
   SKELETON = 'skeleton'
 }
 
-
 export function PatientForm() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const router = useRouter()
-
-  const [isLoading,setIsLoading] = useState(false)
-
-
-  // 1. Define your form.
   const form = useForm<z.infer<typeof UserFormSchema>>({
     resolver: zodResolver(UserFormSchema),
     defaultValues: {
       name: "",
       email: "",
-      phone : ""
+      phone: ""
     },
-  })
- 
-  // 2. Define a submit handler.
-  async function onSubmit({name,email,phone}: z.infer<typeof UserFormSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    setIsLoading(true)
+  });
+
+  async function onSubmit(values: z.infer<typeof UserFormSchema>) {
+    setIsLoading(true);
 
     try {
-      const userData = {
-        name,
-        email,
-        phone
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
       }
-
-      const user = await createUser(userData);
-
-      if(user) router.push(`/patients/${user.$id}/register`)
-
-
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-    
-  }
+
+    setIsLoading(false);
+  };
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4 ">
+        <section className="mb-12 space-y-4">
           <h1 className="header">
             Hi there👋
           </h1>
@@ -76,42 +69,40 @@ export function PatientForm() {
           </p>
         </section>
 
-        <CustomFormProvider 
-          fieldType = {FormFieldType.INPUT}
+        <CustomFormProvider
+          fieldType={FormFieldType.INPUT}
           control={form.control}
           name="name"
           label="Full Name"
           placeholder="Enter your full name"
           iconSrc="/assets/icons/user.svg"
-          iconAlt = "users"
+          iconAlt="users"
         />
 
-        <CustomFormProvider 
-          fieldType = {FormFieldType.INPUT}
+        <CustomFormProvider
+          fieldType={FormFieldType.INPUT}
           control={form.control}
           name="email"
           label="Email"
           placeholder="Enter your Email"
           iconSrc="/assets/icons/email.svg"
-          iconAlt = "email"
+          iconAlt="email"
         />
 
-        <CustomFormProvider 
-          fieldType = {FormFieldType.PHONE_INPUT}
+        <CustomFormProvider
+          fieldType={FormFieldType.PHONE_INPUT}
           control={form.control}
           name="phone"
           label="Phone Number"
           placeholder="Enter your Number"
-          // iconSrc="/assets/icons/phone.svg"
-          // iconAlt = "users"
         />
-        
-        <SubmitButton isLoading={isLoading} >
+
+        <SubmitButton isLoading={isLoading}>
           Get Started
         </SubmitButton>
       </form>
     </Form>
-  )
+  );
 }
 
-export default PatientForm
+export default PatientForm;
